0cd8aa76e4b24087f1701f73b46d6c95c85b3f2a
Idedav <xdavide.correa@gmail.com>
2024-06-30 17:06:03 +0200

    INIT project

---
+++ ecommerceOn/src/main/java/com/ecommerceOn/ecommerceOn/EcommerceOnApplication.java
@@ -0,0 +1,13 @@
+package com.ecommerceOn.ecommerceOn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceOnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceOnApplication.class, args);
+	}
+
+}
---
+++ ecommerceOn/src/test/java/com/ecommerceOn/ecommerceOn/EcommerceOnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommerceOn.ecommerceOn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceOnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
